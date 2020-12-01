3d95cddf38daf6954b0052a38872c0e6da9c144f
Yilber Patiño <yilberesaupm@ufps.edu.co>
2020-12-01 08:37:31 -0500

    Subiendo a servidor

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
 
-define('URL','http://localhost/proyectosPHP/ordersoft-ayd/');
-define('HOST','localhost');
-define('DB','ordersoft');
-define('USER','root');
-define('PASSWORD','');
+define('URL','http://ordersoft.ayd.ingsistemasufps.co/');
+define('HOST','ordersoft.ayd.ingsistemasufps.co');
+define('DB','ordersoft_osb');
+define('USER','ordersoft_ayd');
+define('PASSWORD','1155705');
 define('CHARSET','utf8');
 
 ?>
