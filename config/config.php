7e2ae7200a71e693b2de9627ce2ffdf1886937b5
Yilber Patiño <yilberesaupm@ufps.edu.co>
2020-12-02 11:17:59 -0500

    host

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('URL','http://ordersoft.ayd.ingsistemasufps.co/');
-define('HOST','ordersoft.ayd.ingsistemasufps.co');
+define('HOST','ingsistemasufps.co');
 define('DB','ordersoft_osb');
 define('USER','ordersoft_ayd');
 define('PASSWORD','1155705');
